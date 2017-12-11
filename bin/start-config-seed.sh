775389e74c9dc298329d0b78eb6a18a141fd8857
Tony Espy <espy@canonical.com>
2017-12-11 18:46:47 -0500

    [WIP] Fix config-seed startup

--- bin/start-config-seed.sh
+++ bin/start-config-seed.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
-set -e
+set -x
 
 count=10
 
 while [ "$count" -gt 0 ] ; do
+    CONSUL_RUNNING=`curl http://127.0.0.1:8500/v1/catalog/service/consul`
 
-    CONSUL_RUNNING=`curl http://127.0.1.1:8500/v1/catalog/service/consul`
-
-    if [ $CONSUL_RUNNING = "[]" ] ; then
+    if [ $? -ne 0] || [ $CONSUL_RUNNING = "[]" ] || [ $CONSUL_RUNNING = "" ]; then
 	sleep 20
     else
 	break
     fi
 done
 
-echo "CONSUL_RUNNING=$CONSUL_RUNNING"
-
 # start config-seed if consul is up
+#
+# TODO: this success check could be improved...
 if [ $CONSUL_RUNNING != "[]" ] ; then
     cd $SNAP/jar/config-seed/
 
-    exec $SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+    $SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
     $SNAP/jar/config-seed/core-config-seed.jar
 fi
 
