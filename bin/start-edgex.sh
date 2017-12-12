ad3c8ed35d29a44a3c7c1f54a6027e30dbffadc5
Tony Espy <espy@canonical.com>
2017-12-11 19:54:23 -0500

    [WIP] Add core-data to start-edgex script

--- bin/start-edgex.sh
+++ bin/start-edgex.sh
@@ -15,4 +15,6 @@ fi
 
 $SNAP/mongo/launch-edgex-mongo.sh
 
-
+echo "Starting core-data"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/core-data/core-data.jar &
