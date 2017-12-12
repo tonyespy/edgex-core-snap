5116581b3381d014aefd0c17119f80191b2434e8
Tony Espy <espy@canonical.com>
2017-12-12 07:51:13 -0500

    [WIP] Add remaining services to start-edgex script

--- bin/start-edgex.sh
+++ bin/start-edgex.sh
@@ -15,6 +15,54 @@ fi
 
 $SNAP/mongo/launch-edgex-mongo.sh
 
+echo "Starting logging"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/support-logging/support-logging.jar &
+
+sleep 65
+
+echo "Starting notifications"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/support-notifications/support-notifications.jar &
+
+sleep 33
+
+echo "Starting metadata"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/core-metadata/core-metadata.jar &
+
+sleep 60
+
 echo "Starting core-data"
 $SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
 $SNAP/jar/core-data/core-data.jar &
+
+sleep 60
+
+echo "Starting command"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/core-command/core-command.jar &
+
+sleep 60
+
+echo "Starting scheduler"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/support-scheduler/support-scheduler.jar &
+
+sleep 60
+
+echo "Starting export-client"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/export-client/export-client.jar &
+
+sleep 60
+
+echo "Starting export-distro"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/export-distro/export-distro.jar &
+
+sleep 60
+
+echo "Starting device-virtual"
+$SNAP/jre/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+$SNAP/jar/device-virtual/device-virtual.jar &
