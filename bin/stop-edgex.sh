3af4adedbbff57c3102ce11e836bdff255bddf9a
Tony Espy <espy@canonical.com>
2018-02-14 16:18:07 -0500

    stop-edgex: use SIGINT for clean consul shutdown

--- bin/stop-edgex.sh
+++ bin/stop-edgex.sh
@@ -5,9 +5,15 @@ set -ex
 # and fell back to SIGKILL, however this script is not meant
 # to be long-lived, hopefully deprecated once everything
 # all micro services becomes snap daemons.
-stop_service() {
-	echo "killing $2 ($1) service"
+kill_service() {
+	echo "sending SIGKILL to $2 ($1) service"
 	kill -9 $1
+}
+
+# send SIGINT to consul
+int_service() {
+	echo "sending SIGINT to $2 ($1) service"
+	kill -INT $1
 }
 
 # Bootstrap service env vars
@@ -21,7 +27,7 @@ if [ $DEVICE_VIRTUAL = "y" ] ; then
     pid=`ps -ef | grep device-virtual | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "device-virtual"
+	kill_service $pid "device-virtual"
     fi
 fi
 
@@ -29,7 +35,7 @@ if [ $EXPORT_DISTRO = "y" ] ; then
     pid=`ps -ef | grep export-distro | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "export-distro"
+	kill_service $pid "export-distro"
     fi
 fi
 
@@ -37,7 +43,7 @@ if [ $EXPORT_CLIENT = "y" ] ; then
     pid=`ps -ef | grep export-client | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "export-client"
+	kill_service $pid "export-client"
     fi
 fi
 
@@ -45,7 +51,7 @@ if [ $SUPPORT_SCHEDULER = "y" ] ; then
     pid=`ps -ef | grep support-scheduler | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "support-scheduler"
+	kill_service $pid "support-scheduler"
     fi
 fi
 
@@ -53,7 +59,7 @@ if [ $CORE_COMMAND = "y" ] ; then
     pid=`ps -ef | grep core-command | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "core-command"
+	kill_service $pid "core-command"
     fi
 fi
 
@@ -61,7 +67,7 @@ if [ $CORE_DATA = "y" ] ; then
     pid=`ps -ef | grep core-data | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "core-data"
+	kill_service $pid "core-data"
     fi
 fi
 
@@ -69,7 +75,7 @@ if [ $CORE_METADATA = "y" ] ; then
     pid=`ps -ef | grep core-metadata | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "core-metadata"
+	kill_service $pid "core-metadata"
     fi
 fi
 
@@ -77,7 +83,7 @@ if [ $SUPPORT_NOTIFICATIONS = "y" ] ; then
     pid=`ps -ef | grep support-notifications | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "support-notifications"
+	kill_service $pid "support-notifications"
     fi
 fi
 
@@ -85,7 +91,7 @@ if [ $SUPPORT_LOGGING = "y" ] ; then
     pid=`ps -ef | grep support-logging | grep -v grep | awk '{print $2}'`
 
     if [ ! -z $pid ] && [ $pid != "" ] ; then
-	stop_service $pid "support-logging"
+	kill_service $pid "support-logging"
     fi
 fi
 
@@ -100,5 +106,8 @@ fi
 
 pid=`ps -ef | grep "consul\ agent" | grep -v grep | awk '{print $2}'`
 if [ ! -z $pid ] && [ $pid != "" ] ; then
-    $SNAP/bin/consul leave
+
+    # use SIGINT to gracefully shutdown
+    echo "shutting down consul..."
+    int_service $pid "consul"
 fi
