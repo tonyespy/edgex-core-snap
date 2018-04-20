ea59a811e782f74403113f46b221b1e7d6e4ee5d
Ian Johnson <ian.johnson@canonical.com>
2018-04-20 08:58:32 -0500

    Making mongodb a daemon
    This change involves splitting the current mongodb part into two scripts, a server daemon script which actually runs the daemon (and doesn't fork), and a client connect script which runs the necessary javascript init file from docker-edgex-mongo.
    Due to this change, removing the code from snapcraft.yaml which did replacements on the original script from docker-edgex-mongo and just adding in the desired script here.

--- bin/start-edgex.sh
+++ bin/start-edgex.sh
@@ -24,19 +24,11 @@ $SNAP/bin/start-consul.sh
 
 sleep 60
 
-MONGO_DATA_DIR=$SNAP_DATA/mongo/db
-
 echo "Starting config-seed..."
 $SNAP/bin/start-config-seed.sh
 
-echo "Starting mongo..."
-if [ -e $MONGO_DATA_DIR ] ; then
-    rm -rf $MONGO_DATA_DIR/*
-else
-    mkdir -p $MONGO_DATA_DIR
-fi
-
-$SNAP/mongo/launch-edgex-mongo.sh
+echo "Connecting to mongo..."
+$SNAP/mongo/connect-edgex-mongo.sh
 
 if [ $SUPPORT_LOGGING = "y" ] ; then
     sleep 60
